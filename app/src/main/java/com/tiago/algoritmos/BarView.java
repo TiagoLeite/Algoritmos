69739101be555b5b36ef6fbb83a0e23673dfebbe
TiagoLeite <tiago.tmleite@gmail.com>
2017-08-20 10:30:57 -0300

    charts bar

---
+++ app/src/main/java/com/tiago/algoritmos/BarView.java
@@ -0,0 +1,10 @@
+package com.tiago.algoritmos;
+
+import View;
+
+/**
+ * Created by Techfit on 20/08/2017.
+ */
+
+public class BarView extends View {
+}
---
+++ app/src/main/java/com/tiago/algoritmos/BubbleSortFragment.java
@@ -0,0 +1,10 @@
+package com.tiago.algoritmos;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Techfit on 20/08/2017.
+ */
+
+public class BubbleSortFragment extends Fragment {
+}
