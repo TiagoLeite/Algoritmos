860217e8514642aa8b114b713832bf71ad2f7bcf
TiagoLeite <tiago.tmleite@gmail.com>
2017-08-27 11:43:36 -0300

    button positions

--- app/src/main/java/com/tiago/algoritmos/BubbleSortFragment.java
+++ app/src/main/java/com/tiago/algoritmos/BubbleSortFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +34,7 @@ public class BubbleSortFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.frag_bubble, container, false);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         getActivity().setTitle("BubbleSort");
 
